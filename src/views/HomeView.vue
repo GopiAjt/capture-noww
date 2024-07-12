d53722ee37d8a603f89487e25b09960d1317d72f
GopiAjt <gopiajt23@gmail.com>
2024-07-12 22:39:47 +0530

    add: installed primevue for UI

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
+<template>
+  <main>
+    <h1>This is Home Tab</h1>
+  </main>
+</template>
+
 <script setup>
 
 </script>
-
-<template>
-  <main>
-      <h1>This is Home Tab</h1>    
-  </main>
-</template>
