3ae85329742d7fca375a554ea4ecad34f87a2e7c
GopiAjt <gopiajt23@gmail.com>
2024-08-16 23:37:16 +0530

    add: loading screen for booking

--- src/components/Booking.vue
+++ src/components/Booking.vue
@@ -12,10 +12,12 @@
     </div>
     <Button @click="book" label="Book" outlined fluid></Button>
     <Toast position="bottom-center" />
+    <LoadingScreen :isVisible="isLoading" ></LoadingScreen>
 </template>
 
 <script>
 import AuthService from '@/services/AuthService';
+import LoadingScreen from '@/components/LoadingScreen.vue'
 
 export default {
     props: {
@@ -31,12 +33,41 @@ export default {
     data() {
         return {
             fromDate: null,
-            tillDate: null
+            tillDate: null,
+            isLoading: false
         };
     },
-
+    components: {
+        LoadingScreen
+    },
     methods: {
+        validateFields() {
+            if (!this.fromDate) {
+                this.$toast.add({ severity: 'warn', summary: 'From Date is required', life: 3000 });
+                return false;
+            }
+            if (!this.tillDate) {
+                this.$toast.add({ severity: 'warn', summary: 'Till Date is required', life: 3000 });
+                return false;
+            }
+            if (this.fromDate >= this.tillDate) {
+                this.$toast.add({ severity: 'warn', summary: 'From Date must be earlier than Till Date', life: 3000 });
+                return false;
+            }
+            if (!this.package) {
+                this.$toast.add({ severity: 'warn', summary: 'Please Select a Package', life: 3000 });
+                return false;
+            }
+            if (!this.photographer_id) {
+                this.$toast.add({ severity: 'warn', summary: 'Photographer ID is required', life: 3000 });
+                return false;
+            }
+            return true;
+        },
         async book() {
+            if (!this.validateFields()) {
+                return;
+            }
             console.log(this.fromDate);
             console.log(this.tillDate);
             console.log(this.package);
@@ -44,18 +75,21 @@ export default {
             console.log(this.$store.state.token);
 
             try {
-                const response = await AuthService.bookPackage(this.fromDate, 
-                                                                this.tillDate, 
-                                                                this.package,
-                                                                this.$store.state.user.email, 
-                                                                this.photographer_id, 
-                                                                this.$store.state.token);
+                this.isLoading = true;
+                const response = await AuthService.bookPackage(this.fromDate,
+                    this.tillDate,
+                    this.package,
+                    this.$store.state.user.email,
+                    this.photographer_id,
+                    this.$store.state.token);
 
                 this.$toast.add({ severity: 'success', summary: 'Booked successfully!', life: 3000 });
                 console.log(response);
+                this.isLoading = false;
             } catch (error) {
                 this.$toast.add({ severity: 'error', summary: 'Error! please try again later', life: 3000 });
                 console.log(error);
+                this.isLoading = false;
             }
             console.log("booked");
         }
--- src/components/LoadingScreen.vue
+++ src/components/LoadingScreen.vue
@@ -1,33 +1,38 @@
 <template>
-    <div class="loading-container ">
+    <div v-if="isVisible" class="loading-container">
         <div class="card flex justify-center">
             <ProgressSpinner />
         </div>
-
-        <l-infinity size="55" stroke="4" stroke-length="0.15" bg-opacity="0.1" speed="1.3" color="black"></l-infinity>
-
     </div>
 </template>
+
 
 <script>
 
 
-
+export default {
+    props: {
+        isVisible: {
+            type: Boolean,
+            required: true
+        }
+    }
+};
 
 // Default values shown
 
 </script>
 <style scoped>
-
 .loading-container {
-    display: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     position: fixed;
     top: 0;
     left: 0;
     width: 100%;
     height: 100%;
     background-color: rgba(0, 0, 0, 0.7);
-    filter: blur(50%);
     z-index: 9999;
 }
 </style>
