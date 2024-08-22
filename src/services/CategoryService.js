dca68690caf07811294833b4a0c3a29007a480f9
GopiAjt <gopiajt23@gmail.com>
2024-08-22 23:20:27 +0530

    add: categories to this service

--- src/services/CategoryService.js
+++ src/services/CategoryService.js
@@ -1,21 +1,26 @@
 export const CategoryService = {
     getData() {
         return [
-            { name: 'Afghanistan', code: 'AF' },
-            { name: 'Albania', code: 'AL' },
-            { name: 'Algeria', code: 'DZ' },
-            { name: 'American Samoa', code: 'AS' },
-            { name: 'Andorra', code: 'AD' },
-            { name: 'Angola', code: 'AO' },
-            { name: 'Anguilla', code: 'AI' },
-            { name: 'Antarctica', code: 'AQ' },
-            { name: 'Antigua and Barbuda', code: 'AG' },
-            { name: 'Argentina', code: 'AR' },
-            { name: 'Armenia', code: 'AM' },
-            { name: 'Aruba', code: 'AW' },
-            { name: 'Australia', code: 'AU' },
-            { name: 'Austria', code: 'AT' },
-            { name: 'Azerbaijan', code: 'AZ' }
+            { name: 'wedding', code: 'AF' },
+            { name: 'pre wedding', code: 'AL' },
+            { name: 'Portraits', code: 'DZ' },
+            { name: 'Documentary', code: 'AS' },
+            { name: 'Fashion', code: 'AD' },
+            { name: 'Street', code: 'AO' },
+            { name: 'Event', code: 'AI' },
+            { name: 'Travel', code: 'AQ' },
+            { name: 'Pet', code: 'AG' },
+            { name: 'Product', code: 'AR' },
+            { name: 'Real Estate', code: 'AM' },
+            { name: 'Food', code: 'AW' },
+            { name: 'Still Life', code: 'AU' },
+            { name: 'Architecture', code: 'AT' },
+            { name: 'Landscape', code: 'AZ' },
+            { name: 'Wildlife', code: 'AZ' },
+            { name: 'Macro', code: 'AZ' },
+            { name: 'Astrophotography', code: 'AZ' },
+            { name: 'Scientific', code: 'AZ' },
+            { name: 'Underwater', code: 'AZ' }
         ];
     },
 
