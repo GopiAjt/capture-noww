dcba17fda795bf0ba27cb9a2e01b0af1a71dcf9a
GopiAjt <gopiajt23@gmail.com>
2024-08-25 22:15:04 +0530

    add:  T&C data and content

--- src/components/footerComponents/TermsAndConditions.vue
+++ src/components/footerComponents/TermsAndConditions.vue
@@ -1,13 +1,185 @@
 <template>
-    <h1>Terms and conditions</h1>
+    <NavBar></NavBar>
+    <div class="data">
+        <h1 style="font-weight: bolder;">Terms and Conditions </h1>
+        <h3> Introduction:</h3>
+        <ol>
+            <li> By accessing and using CaptureNow.in services, you agree to comply with these terms and
+                conditions. </li>
+            <li>CaptureNow.in reserves the right to modify the terms at any time, and such changes will be
+                effective upon posting</li>
+        </ol>
+        <h3> Service Overview:</h3>
+        <ol>
+            <li>CaptureNow.in connects customers with photographers based on performance, work, and budget
+                preferences.</li>
+            <li>Customers can book photographers for various events and sessions through the platform.
+            </li>
+        </ol>
+        <h3> User Responsibilities:</h3>
+        <ol>
+            <li>Users must provide accurate and truthful information during registration and booking processes.
+            </li>
+            <li>Users are responsible for maintaining the confidentiality of their account information.</li>
+
+        </ol>
+        <h3> Booking and Payments:</h3>
+        <ol>
+            <li>Users agree to pay the agreed-upon fees for photographer services.</li>
+            <li>CaptureNow.in may charge a service fee for facilitating bookings.</li>
+            <li> Payment terms, including deposits and cancellations, are outlined in the booking process.</li>
+        </ol>
+
+        <h3>Photographer Selection:</h3>
+
+        <ol>
+            <li>CaptureNow.in strives to showcase photographers’ work accurately, but variations may occur.</li>
+
+            <li>Users are encouraged to communicate preferences and expectations clearly during the booking
+                process.</li>
+        </ol>
+        <h3>Quality Assurance:</h3>
+        <ol>
+            <li>CaptureNow.in is committed to ensuring the quality of services provided by photographers.</li>
+
+            <li>Users can provide feedback and reviews to help maintain service standards.</li>
+
+        </ol>
+        <h3>Cancellations and Refund:</h3>
+        <ol>
+            <li> Cancellation policies are specified during the booking process.</li>
+
+            <li>Refunds, if applicable, are subject to the terms outlined in the cancellation policy.</li>
+        </ol>
+
+        <h3>Intellectual Property:</h3>
+        <ol>
+            <li>Photographers retain the rights to their work, and users must respect copyright laws.</li>
+            <li>Users grant CaptureNow.in the right to use their event/session photos for promotional purposes.</li>
+        </ol>
+        <h3>Privacy Policy:</h3>
+        <ol>
+            <li>CaptureNow.in values user privacy and adheres to a strict privacy policy.</li>
+            <li>Personal information is collected and used in accordance with the privacy policy.</li>
+        </ol>
+        <h3>Limitation of Liability:</h3>
+        <ol>
+            <li>CaptureNow.in is not liable for any damages arising from the use of its services.</li>
+            <li>Users engage photographers at their own risk.</li>
+
+        </ol>
+        <h3>Termination:</h3>
+        <ol>
+            <li>CaptureNow.in reserves the right to terminate user accounts for violations of these terms.</li>
+            <li>Users can terminate their accounts by following the specified procedures.</li>
+        </ol>
+
+        <h3>Governing Law:</h3>
+        <ol>
+            <li>These terms and conditions are governed by the laws of [Jurisdiction].</li>
+        </ol>
+        <h3>Contact Information:</h3>
+        <ol>
+            <li> For inquiries or concerns, users can contact CaptureNow.in at [contact email/phone].</li>
+            <li> By using CaptureNow.in, you acknowledge and agree to these terms and conditions.</li>
+        </ol>
+
+        <h1 style="font-weight: bolder;">Data Policy:</h1>
+        <h3>Data Collection and Usage:</h3>
+        <ol>
+            <li> CaptureNow.in collects user information to facilitate the booking process and enhance user
+                experience.</li>
+            <li> Information may include personal details, event specifications, and communication history.</li>
+        </ol>
+        <h3>User Consent:</h3>
+        <ol>
+            <li> By using CaptureNow.in, users consent to the collection, processing, and storage of their data
+                as outlined in this policy.</li>
+            <li> Users have the right to withdraw consent or request data deletion.</li>
+        </ol>
+        <h3>Data Security:</h3>
+        <ol>
+            <li>CaptureNow.in employs industry-standard measures to protect user data from unauthorized access,
+                disclosure, alteration, and destruction.</li>
+            <li>Secure transmission methods are used to safeguard data during transfer.</li>
+        </ol>
+        <h3>Third-Party Sharing:</h3>
+        <ol>
+            <li>CaptureNow.in may share necessary user information with photographers to facilitate bookings.
+            </li>
+            <li>User data is not shared with third parties for marketing purposes without explicit consent.</li>
+        </ol>
+        <h3>Cookies and Trag:</h3>
+        <ol>
+            <li>CaptureNow.in uses cookies and similar technologies to enhance user experience and analyze
+                website performance.</li>
+            <li>Users can manage cookie preferences through browser settings.</li>
+        </ol>
+        <h3>Retention and Deletion:</h3>
+        <ol>
+            <li>CaptureNow.in retains user data for the duration necessary for the purposes outlined in this
+                policy.</li>
+            <li>Users can request the deletion of their data by contacting CaptureNow.in.</li>
+        </ol>
+        <h3>User Access and Control:</h3>
+        <ol>
+            <li>Users can access and update their personal information through their CaptureNow.in accounts.
+            </li>
+            <li>Users have the right to request information about the data collected and its processing.</li>
+        </ol>
+        <h3>Marketing Communication:</h3>
+        <ol>
+            <li> CaptureNow.in may send users relevant marketing communications based on their preferences.</li>
+            <li>Users can opt-out of marketing communications at any time.</li>
+        </ol>
+        <h3>Compliance with Law:</h3>
+        <ol>
+            <li>CaptureNow.in complies with applicable data protection laws and regulations.
+                Changes to the data policy will be made in accordance with legal requirements.</li>
+        </ol>
+        <h3> Data Breach Notification:</h3>
+        <ol>
+            <li>In the event of a data breach, CaptureNow.in will promptly notify affected users and relevant
+                authorities in accordance with applicable laws.</li>
+        </ol>
+        <h3>Children’s Privacy:</h3>
+        <ol>
+            <li> CaptureNow.in is not intended for use by individuals under the age of 18.</li>
+            <li>If a user is identified as a minor, parental consent may be required for data processing.</li>
+        </ol>
+        <h3>Updates to the Data Policy:</h3>
+        <ol>
+            <li>CaptureNow.in reserves the right to update this data policy. Users will be notified of
+                significant changes.</li>
+        </ol>
+
+        <h3>Contact Information:</h3>
+        <ol>
+            <li>For inquiries or concerns related to data privacy, users can contact CaptureNow.in at "EMPTY
+                need to add"</li>
+            <li>Using CaptureNow.in, you acknowledge and agree to the terms outlined in this data policy.</li>
+        </ol>
+    </div>
+    <Footer></Footer>
 </template>
 
 <script>
 import AuthService from '@/services/AuthService';
+import NavBar from '../NavBar.vue';
+import Footer from '@/components/Footer.vue';
 
+export default {
+    components: {
+        Footer,
+        NavBar,
+    }
+}
 </script>
 
 
 <style scoped>
-
+.data{
+    margin-left: 20px;
+    margin-right: 20px;
+}
 </style>
